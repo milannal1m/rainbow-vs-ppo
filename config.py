8a3c081da773293babb5234ddd8317749fb73f5a
milannal1m <milan.toth@uni-ulm.de>
2026-05-29 11:20:26 +0200

    Second Run on Cluster

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DATE_FORMAT          = "%Y-%m-%d_%H-%M-%S"
 RUNS_DIR             = "runs"
-CHECKPOINT_EVERY     = 10000
+CHECKPOINT_EVERY     = 1000
 REPLAY_MEMORY_SEED   = 23
 GRAPH_UPDATE_SECONDS = 10
 HEADLESS             = os.environ.get("HEADLESS", "0") == "1"

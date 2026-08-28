a9a70afd9823fe1f49c0a4785a171534d62b0137
milannal1m <milan.toth@uni-ulm.de>
2026-08-28 07:37:59 +0200

    PPO eval done

--- evaluate.sh
+++ evaluate.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=32000
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 

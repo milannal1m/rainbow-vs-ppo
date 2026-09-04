53ed3891b8038ecade3a2af1fd579cb9a4738451
milannal1m <milan.toth@uni-ulm.de>
2026-09-04 07:47:38 +0200

    hpos done

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu_a100_il
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=3
-#SBATCH --mem=128000
-#SBATCH --time=48:00:00
+#SBATCH --mem=112000
+#SBATCH --time=24:00:00
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
